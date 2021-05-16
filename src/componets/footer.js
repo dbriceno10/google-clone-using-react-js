f70f1bd2f168bf55a4ae598326a6b5146c975d0c
nombre <daniel.b100726@gmail.com>
2021-05-16 16:40:22 -0400

    maquetando clon con react, agregando cambios menores

--- src/componets/footer.js
+++ src/componets/footer.js
@@ -4,34 +4,36 @@ class Footer extends React.Component {
     render() {
         return(
             <footer>
-                {/* <div className="country">
+                <section className="country">
                     <p>Venezuela</p>
-                </div> */}
-                <ul className="footer-left">
-                    <li>
-                        <a href="#">Sobre Google</a>
-                    </li>
-                    <li>
-                        <a href="#">Publicidad</a>
-                    </li>
-                    <li>
-                        <a href="#">Negocios</a>
-                    </li>
-                    <li>
-                        <a href="#">Cómo funciona la búsqueda</a>
-                    </li>
-                </ul>
-                <ul className="footer-right">
-                    <li>
-                        <a href="#">Privacidad</a>
-                    </li>
-                    <li>
-                        <a href="#">Condiciones</a>
-                    </li>
-                    <li>
-                        <a href="#">Preferencias</a>
-                    </li>
-                </ul>
+                </section>
+                <section className="likns">
+                    <ul className="footer-left">
+                        <li>
+                            <a href="#">Sobre Google</a>
+                        </li>
+                        <li>
+                            <a href="#">Publicidad</a>
+                        </li>
+                        <li>
+                            <a href="#">Negocios</a>
+                        </li>
+                        <li>
+                            <a href="#">Cómo funciona la búsqueda</a>
+                        </li>
+                    </ul>
+                    <ul className="footer-right">
+                        <li>
+                            <a href="#">Privacidad</a>
+                        </li>
+                        <li>
+                            <a href="#">Condiciones</a>
+                        </li>
+                        <li>
+                            <a href="#">Preferencias</a>
+                        </li>
+                    </ul>
+                </section>
             </footer>
         )
     }
